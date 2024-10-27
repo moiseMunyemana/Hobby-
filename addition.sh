1fe282efe3ca21e3ad17f3e40a9322742d94dd29
MOISE MUNYEMANA <munyemanam@gmail.com>
2024-10-27 19:00:11 -0300

    new bash files commited

---
+++ addition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#shell program to add two variables
+echo "enter a number"
+read -p "Enter First Number: " var1
+
+echo "enter a 2nd number"
+
+read -p "Enter Second Number: " var2
+
+sum=$(( $var1 + $var2))
+
+echo "sum is:  $sum"
---
+++ name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+echo "What's your name?" 
+
+read enter_fname 
+
+echo -e "\nWelcome to bash tutorial" $entered_name
+
+read enter_lname
+
+echo $enetred_lname + $ enter_fname
